00981215fe39d8eb64ca6d68ceb1758ba43ddaf4
zhangliang <hackdapp@gmail.com>
2026-02-18 01:01:01 +0800

    Configure basic static search

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,16 +1,16 @@
+export const dynamic = 'force-static';
+
 import { source } from '@/lib/source';
 import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-static';
-
 export async function GET() {
-    const indexes = source.getPages().map((page) => ({
-        title: page.data.title,
-        description: page.data.description,
-        url: page.url,
-        id: page.url,
-        structured: page.data.structuredData,
-    }));
+  const indexes = source.getPages().map((page) => ({
+    title: page.data.title,
+    description: page.data.description,
+    url: page.url,
+    id: page.url,
+    structuredData: page.data.structuredData,
+  }));
 
-    return NextResponse.json(indexes);
+  return NextResponse.json(indexes);
 }
