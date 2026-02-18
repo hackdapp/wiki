ae9054732eb2a3a65e7cf0b5607a6547436a07d1
zhangliang <hackdapp@gmail.com>
2026-02-18 11:51:46 +0800

    Completely revert search changes

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,21 +1,10 @@
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
-import dynamic from 'next/dynamic';
-
-const SearchDialog = dynamic(() => import('@/components/search'), { ssr: false });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <DocsLayout 
-      tree={source.getPageTree()} 
-      {...baseOptions()}
-      sidebar={{
-        enabled: true,
-        collapsible: true,
-      }}
-    >
-        <SearchDialog trigger={undefined} open={undefined} onOpenChange={undefined} />
+    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
       {children}
     </DocsLayout>
   );
